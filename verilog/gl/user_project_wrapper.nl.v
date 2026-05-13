28311c720afb10fc4ee548d2b397304e67dc8100
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 06:23:10 -0700

    Fix LVS: use pnl.v for wrapper (includes power pins)

--- verilog/gl/user_project_wrapper.nl.v
+++ verilog/gl/user_project_wrapper.nl.v
@@ -5,6 +5,14 @@ module user_project_wrapper (user_clock2,
     wbs_cyc_i,
     wbs_stb_i,
     wbs_we_i,
+    vssa2,
+    vdda2,
+    vssa1,
+    vdda1,
+    vssd2,
+    vccd2,
+    vssd1,
+    vccd1,
     analog_io,
     io_in,
     io_oeb,
@@ -24,6 +32,14 @@ module user_project_wrapper (user_clock2,
  input wbs_cyc_i;
  input wbs_stb_i;
  input wbs_we_i;
+ inout vssa2;
+ inout vdda2;
+ inout vssa1;
+ inout vdda1;
+ inout vssd2;
+ inout vccd2;
+ inout vssd1;
+ inout vccd1;
  inout [28:0] analog_io;
  input [37:0] io_in;
  output [37:0] io_oeb;
@@ -38,7 +54,9 @@ module user_project_wrapper (user_clock2,
  input [3:0] wbs_sel_i;
 
 
- ucb_top mprj (.clk(wb_clk_i),
+ ucb_top mprj (.VGND(vssd1),
+    .VPWR(vccd1),
+    .clk(wb_clk_i),
     .reward_valid(la_data_in[1]),
     .rst_n(la_data_in[127]),
     .start(la_data_in[0]),
