86c588c6445a5e5eb6e1ffacb4f6991e541ff0b3
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 08:42:06 -0700

    Fix LVS: simplify conb_1 connections

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:031211bb4173cf652af041a87ead623de274356b4b1e21fea5589fa61ad14fc5
-size 2006
+oid sha256:6291ad7e53464b56261e1d0a26f1ee95ab04e28789857587b93aa9892ff25d3b
+size 1924
