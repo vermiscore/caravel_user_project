0a1ded462e65b41fc6f42486907800fa97502774
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 06:02:09 -0700

    Fix LVS: use pnl.v (with VPWR/VGND) for ucb_top

---
+++ precheck_results/13_MAY_2026___12_56_51/tmp/gds2txt.py
@@ -0,0 +1,24 @@
+import pya
+
+app = pya.Application.instance()
+opt = pya.SaveLayoutOptions()
+layout_view = pya.Layout()
+
+input_layout = "/tmp/tmpj3_gmuiu/repo/gds/user_project_wrapper.gds"
+output = "/tmp/tmpj3_gmuiu/repo/precheck_results/13_MAY_2026___12_56_51/tmp/layout.txt"
+# Setting the name of the output file and setting the substitution character
+print("[INFO] Changing from " + input_layout + "\n	to " + output)
+opt.set_format_from_filename(output)
+opt.oasis_substitution_char=''
+
+# Reading the input file and writing it to the output file name
+layout_view.read(input_layout)
+for cell_it in layout_view.each_cell():
+    if cell_it.name.endswith("user_project_wrapper"):
+       myIndex = layout_view.cell(cell_it.name).cell_index()
+       break
+opt.select_cell(myIndex)
+opt.add_layer(0, pya.LayerInfo())
+layout_view.write(output, opt)
+
+app.exit(0)
