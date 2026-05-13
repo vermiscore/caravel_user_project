794393d02a6a1fd61235f12178bec96bc48dcc33
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 12:19:55 -0700

    Re-harden with io_oeb assign, update pnl.v

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6291ad7e53464b56261e1d0a26f1ee95ab04e28789857587b93aa9892ff25d3b
-size 1924
+oid sha256:6d66d8d3f626e668bb8bfa30efe57848a41b87088a55dc53bdd5a33da528228a
+size 3216
