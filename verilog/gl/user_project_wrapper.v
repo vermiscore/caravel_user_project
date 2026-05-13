437fd677b5afcba03e0d914d44d9490da83ba9d5
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 08:27:40 -0700

    Fix LVS: add conb_1 instances to wrapper.v

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8139487832e834f994b66f4860290c0f7ca3355ead0eb18f4d3cd1b4fd4264b7
-size 1767
+oid sha256:031211bb4173cf652af041a87ead623de274356b4b1e21fea5589fa61ad14fc5
+size 2006
