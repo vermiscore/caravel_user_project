07aa3ce3057012e0eeb2ec97ab41954713aa5b64
ChipFoundry Automation <platform@chipfoundry.io>
2026-05-14 06:01:16 +0000

    chipfoundry: remote precheck results (3b8b337e-1763-4cf1-970c-dfb842b9b854)

---
+++ precheck_results/14_MAY_2026___06_00_04/tmp/gds2txt.py
@@ -0,0 +1,24 @@
+import pya
+
+app = pya.Application.instance()
+opt = pya.SaveLayoutOptions()
+layout_view = pya.Layout()
+
+input_layout = "/tmp/tmp8ueywtw0/repo/gds/user_project_wrapper.gds"
+output = "/tmp/tmp8ueywtw0/repo/precheck_results/14_MAY_2026___06_00_04/tmp/layout.txt"
+# Setting the name of the output file and setting the substitution character
+print("[INFO] Changing from " + input_layout + "\n	to " + output)
+opt.set_format_from_filename(output)
+opt.oasis_substitution_char=''
+
+# Reading the input file and writing it to the output file name
+layout_view.read(input_layout)
+for cell_it in layout_view.each_cell():
+    if cell_it.name.endswith("user_project_wrapper"):
+       myIndex = layout_view.cell(cell_it.name).cell_index()
+       break
+opt.select_cell(myIndex)
+opt.add_layer(0, pya.LayerInfo())
+layout_view.write(output, opt)
+
+app.exit(0)
