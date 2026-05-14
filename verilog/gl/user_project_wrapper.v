ea55cea18f133c537fac37ef21effcf0107a5bd6
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 22:57:11 -0700

    Fix LVS/OEB: use new GDS with conb_1, match SPICE connections

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a9f6f632b4092acd379cda64426230dd26de2406cc69788a2b453d377175a61
-size 3052
+oid sha256:9ae5eccff43160d02980d7f2d131f6dde4c63ffd3ddf858b5dab30800c6a1214
+size 3369
