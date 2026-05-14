02d9f427769a2dc6d0207865457a3348cf7ad8e3
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 23:17:03 -0700

    Fix DRC: use PDK conb_1, rebuild wrapper.v from pnl.v

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ae5eccff43160d02980d7f2d131f6dde4c63ffd3ddf858b5dab30800c6a1214
-size 3369
+oid sha256:fef41f280d74c588d8dcb89f4cd0866c81f42f3085aeb3fffe308e9d817df371
+size 2076
