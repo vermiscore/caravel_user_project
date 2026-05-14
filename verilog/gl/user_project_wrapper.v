5c549dcade34925fc1562aa1607c14594365d9ae
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-13 22:46:01 -0700

    Fix LVS: remove conb_1 from verilog, use direct assign

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d66d8d3f626e668bb8bfa30efe57848a41b87088a55dc53bdd5a33da528228a
-size 3216
+oid sha256:5a9f6f632b4092acd379cda64426230dd26de2406cc69788a2b453d377175a61
+size 3052
